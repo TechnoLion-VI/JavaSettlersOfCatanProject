5f69e0307e62656165335c4f9d61d900dde1b453
am429 <almadhuvarshi@gmail.com>
2022-04-08 08:02:08 -0500

    more classes B)

--- Main/src/Knight.java
+++ Main/src/Knight.java
@@ -1,2 +1,9 @@
-public class Knight {
+public class Knight extends DevelopmentCard{
+    private String type = "Knight";
+    public Knight(){
+        super();
+    }
+    public boolean knightCard(Player p){
+        return true; //not official
+    }
 }
