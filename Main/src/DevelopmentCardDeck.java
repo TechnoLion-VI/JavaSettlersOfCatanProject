eab7f39a278916d9ca1950cedfbc860999a43807
cc594 <charitychern@gmail.com>
2022-04-10 00:32:26 -0500

    wrote part of devCardDeck (didn't do constructor; need card classes first)

--- Main/src/DevelopmentCardDeck.java
+++ Main/src/DevelopmentCardDeck.java
@@ -1,2 +1,12 @@
+import java.util.ArrayList;
+
 public class DevelopmentCardDeck {
+    private ArrayList<DevelopmentCard> deck=new ArrayList<>();
+
+    public DevelopmentCardDeck() {
+
+    }
+    public DevelopmentCard draw() {
+        return deck.remove(0);
+    }
 }
