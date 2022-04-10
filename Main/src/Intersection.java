abfed5bf8808c20294b1ef707a99ec0a8526d79d
cc594 <charitychern@gmail.com>
2022-04-10 14:50:43 -0500

    finished basic variables and methods

--- Main/src/Intersection.java
+++ Main/src/Intersection.java
@@ -1,2 +1,51 @@
+import java.awt.image.BufferedImage;
+
 public class Intersection {
+    private boolean isStlmt, isCity, isHarbor;
+    private Player owner;
+    private Edge[] edges=new Edge[3];
+    private BufferedImage stlmt, city;
+    private int[] location =new int[2];
+
+    public Intersection(int x, int y) {
+        location[0]=x;
+        location[1]=y;
+        //add the initializing of edges
+    }
+    public int[] getLocation() {
+        return location;
+    }
+    public boolean isStlmt() {
+        return isStlmt;
+    }
+    public boolean isCity() {
+        return isCity;
+    }
+    public boolean isHarbor() {
+        return isHarbor;
+    }
+    public Player getOwner() {
+        return owner;
+    }
+    public Edge[] getEdges() {
+        return edges;
+    }
+    public BufferedImage getImage() {
+        if (isCity) {
+            return city;
+        }
+        else if (isStlmt) {
+            return stlmt;
+        }
+        else return null;
+    }
+
+    public boolean canPlace() {
+        for (Edge e:edges) {
+            if (e.hasIntersection())
+                return false;
+        }
+        return true;
+    }
+    public
 }
