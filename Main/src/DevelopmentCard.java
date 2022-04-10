bfa1c1f827cc93ea0843152bc4567818d2526df0
cc594 <charitychern@gmail.com>
2022-04-10 00:31:32 -0500

    wrote devCard (basic)

--- Main/src/DevelopmentCard.java
+++ Main/src/DevelopmentCard.java
@@ -1,2 +1,9 @@
 public class DevelopmentCard {
+    private String type;
+    public DevelopmentCard(String s) {
+        type=s;
+    }
+    public String getType() {
+        return type;
+    }
 }
