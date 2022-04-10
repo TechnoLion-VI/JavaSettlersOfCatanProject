1aae232601345303df16ae6ed2876bad37874788
Hey-Sri <vallabhsr@gmail.com>
2022-04-10 16:06:01 -0500

    comments

--- Main/src/ResourceCard.java
+++ Main/src/ResourceCard.java
@@ -6,11 +6,11 @@ public class ResourceCard {
     public ResourceCard(String type, BufferedImage img) {
         this.type = type; this.img = img;
     }
-
+    //type getter
     public String getType() {
         return type;
     }
-
+    //image getter
     public BufferedImage getImg() {
         return img;
     }
