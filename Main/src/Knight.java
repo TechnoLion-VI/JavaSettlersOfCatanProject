4b6a08961104e5c5c840466e3d1d0e810ad1853e
cc594 <charitychern@gmail.com>
2022-04-10 00:33:44 -0500

    fixed some stuff and took out some stuff (should check if the action is legal in gamestate so i took out return type boolean)

--- Main/src/Knight.java
+++ Main/src/Knight.java
@@ -1,18 +1,11 @@
 public class Knight extends DevelopmentCard{
     private String type = "Knight";
     public Knight(){
-        super();
+        super("knight");
     }
-    public boolean knightCard(Player p){
-        boolean canPlay = isLegal(p);
-        if(canPlay == true){
-            p.playedKnightCards++;
+    public void knightCard(Player p){
+            p.addToPlayedKnightCards();
             //allow the player to choose where they want to move the robber
             //set object chosenHex to where they chose and make sure it's different from where the robber was at initially
-            return true;
-        }
-        else{
-            return false;
-        }
     }
 }
