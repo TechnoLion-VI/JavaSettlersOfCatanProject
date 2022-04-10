bf296da37fb602052dbadee2493298f06b72dfd0
Hey-Sri <vallabhsr@gmail.com>
2022-04-10 15:17:00 -0500

    Added flexibility to Edge methods

--- Main/src/Edge.java
+++ Main/src/Edge.java
@@ -5,13 +5,13 @@ public class Edge {
     int[] point1;
     int[] point2;
 
-    public Edge(int x1, int y1, int x2, int y2, Intersection a, Intersection b) {
+    public Edge(int x1, int y1, int x2, int y2, Intersection i1, Intersection i2) {
         owner = null;
-        intersections = new Intersection[]{a, b};
+        setIntersections(i1, i2);
         point1 = new int[]{x1, y1};
         point2 = new int[]{x2, y2};
     }
-
+    //determines if the player can place a road on this edge
     public boolean canPlace(Player p) {
         if (owner != null) return false;
         for (Intersection i:intersections) {
@@ -22,27 +22,50 @@ public class Edge {
         }
         return false;
     }
-
+    //returns true if one of the intersections are owned
     public boolean hasIntersection() {
         for (Intersection i:intersections) {
-            if (i.getOwner() != null) return false;
+            if (i.getOwner() != null) return true;
         }
-        return true;
+        return false;
     }
-
+    //getter and setter for location
     public int[][] getLocation() {
         return new int[][]{point1, point2};
     }
-    public void setLocation(int[] p1, int[] p2) {
-        point1 = p1;
-        point2 = p2;
+    public void setPoint1(int[] p) {
+        point1 = p;
     }
-
+    public void setPoint1(int x1, int y1) {
+        point1 = new int[]{x1, y1};
+    }
+    public void setPoint2(int[] p) {
+        point2 = p;
+    }
+    public void setPoint2(int x2, int y2) {
+        point2 = new int[]{x2, y2};
+    }
+    public void setLocation(int[] p1, int[] p2) {
+        setPoint1(p1); setPoint2(p2);
+    }
+    public void setLocation(int x1, int y1, int x2, int y2) {
+        setPoint1(x1, y1); setPoint2(x2, y2);
+    }
+    //getter and setter for owner variable
     public Player getOwner() {
         return owner;
     }
-
+    public void setOwner(Player p) {
+        owner = p;
+    }
+    //getter and setter for intersections array
     public Intersection[] getIntersections() {
         return intersections;
+    }
+    public void setIntersections(Intersection[] i) {
+        intersections = i;
+    }
+    public void setIntersections(Intersection i1, Intersection i2) {
+        intersections = new Intersection[]{i1, i2};
     }
 }
