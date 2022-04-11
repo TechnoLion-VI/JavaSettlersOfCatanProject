70b503ee81a05b81b4dae5b0173f212670945199
am429 <almadhuvarshi@gmail.com>
2022-04-11 08:02:28 -0500

    trade manager class

--- Main/src/TradeManager.java
+++ Main/src/TradeManager.java
@@ -7,14 +7,16 @@ public class TradeManager {
     }
 
     public boolean domesticTrade(Player p1, Player p2, ArrayList offer1, ArrayList offer2){
-        ArrayList<ResourceCard> rc1;
+        ArrayList<ResourceCard> rc1 = p1.getResourceCards();
+        ArrayList<ResourceCard> rc2 = p2.getResourceCards();
+        return true; //temporary
     }
 
     public boolean maritimeTrade(ArrayList offer, ResourceCard need){
-
+        return true; //temporary
     }
 
     public boolean harborTrade(Edge e, ResourceCard need){
-
+        return true; //temporary
     }
 }
