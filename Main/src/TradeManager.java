7a90acfef05eb255db950250ab9500c643cfe1ad
Charity <charitychern@gmail.com>
2022-04-12 08:02:46 -0500

    added some checks to domestic trade

--- Main/src/TradeManager.java
+++ Main/src/TradeManager.java
@@ -6,9 +6,20 @@ public class TradeManager {
 
     }
 
-    public boolean domesticTrade(Player p1, Player p2, ArrayList offer1, ArrayList offer2){
+    public boolean domesticTrade(Player p1, Player p2, ArrayList<ResourceCard> offer1, ArrayList<ResourceCard> offer2){
         ArrayList<ResourceCard> rc1 = p1.getResourceCards();
         ArrayList<ResourceCard> rc2 = p2.getResourceCards();
+        //needs to check types of resource cards in the offers
+        for (ResourceCard rc: offer1) { //checks player 1 has the resource cards needed
+            if (!rc1.contains(rc))
+                return false;
+            rc1.remove(rc); //removes the card so it makes sure player has the amount needed in case they offer more than 1
+        }
+        for (ResourceCard rc: offer2) {
+            if (!rc2.contains(rc))
+                return false;
+            rc2.remove(rc);
+        }
         return true; //temporary
     }
 
