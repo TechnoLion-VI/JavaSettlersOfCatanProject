24047562fe5c86869400c1baeec0b3bb14c33cc6
Charity <charitychern@gmail.com>
2022-04-14 08:00:45 -0500

    finished use()

--- Main/src/YearOfPlenty.java
+++ Main/src/YearOfPlenty.java
@@ -2,9 +2,12 @@ public class YearOfPlenty extends DevelopmentCard {
     public YearOfPlenty() {
         super("YearOfPlenty");
     }
-    public boolean use(Player p, ResourceCard type1, ResourceCard type2) {
-        if (GameState.resourceDeck.getNumLeft(type1)<=0||GameState.resourceDeck.getNumLeft(type2)<=0)
+    public boolean use(ResourceCard type1, ResourceCard type2) {
+        if (GameState.resourceDeck.getNumLeft(type1)<=0||GameState.resourceDeck.getNumLeft(type2)<=0)   //checks deck has enough
             return false;
-        //needs give() in resourceDeck to be coded first
+        if (type1==type2 && GameState.resourceDeck.getNumLeft(type1)<=1)    //checks it has enough if both are same type
+            return false;
+        GameState.currentPlayer.getResourceCards().add(type1);
+        GameState.currentPlayer.getResourceCards().add(type2);
     }
 }
