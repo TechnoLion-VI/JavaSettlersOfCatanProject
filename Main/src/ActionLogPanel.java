9805c543d0429892c6badf9c6278775f0a358ac5
am429 <almadhuvarshi@gmail.com>
2022-04-18 14:16:13 -0500

    wooo

--- Main/src/ActionLogPanel.java
+++ Main/src/ActionLogPanel.java
@@ -44,7 +44,7 @@ public class ActionLogPanel {
     public void paint(Graphics g){
         g.drawString(GameState.currentPlayer.toString() + " rolled a " + GameState.getDiceNum(), 10, 0); //if dice rolled
         g.drawString(GameState.currentPlayer.toString() + " traded " + TradeManager.p1offer, 10, 5);
-        g.drawString(GameState.currentPlayer.toString() + " built a " , 10, 10);
+        g.drawString(GameState.currentPlayer.toString() + " built a " , 10, 10); //how do we check if they built a city or stlmnt
         g.drawString(GameState.currentPlayer.toString() + "bought a development card.", 10, 15);
         g.drawString(GameState.currentPlayer.toString() + " acquired the longest army card.", 10, 20);
         g.drawString(GameState.currentPlayer.toString() + " acquired the longest road card.", 10, 25);
