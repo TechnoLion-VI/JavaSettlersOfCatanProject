6fe605fc5f8f9e9faf8d33bd12a571de84dcddd8
am429 <almadhuvarshi@gmail.com>
2022-04-18 14:07:37 -0500

    wooo

--- Main/src/ActionLogPanel.java
+++ Main/src/ActionLogPanel.java
@@ -15,6 +15,11 @@ public class ActionLogPanel {
         f.setSize(400, 400); //not final, just a filler
         f.setLayout(null);
         f.setVisible(true);
+        /*panel.AutoScroll = false;
+        panel.HorizontalScroll.Enabled = false;
+        panel.HorizontalScroll.Visible = false;
+        panel.HorizontalScroll.Maximum = 0;
+        panel.AutoScroll = true;/*
 
     }
 
@@ -22,6 +27,7 @@ public class ActionLogPanel {
         System.out.println(GameState.currentPlayer.toString() + " rolled a " + GameState.getDiceNum());
 
         /*
+        roll dice
         trade
         build/buy
         use development card
@@ -36,6 +42,13 @@ public class ActionLogPanel {
     }
 
     public void paint(Graphics g){
-
+        g.drawString(GameState.currentPlayer.toString() + " rolled a " + GameState.getDiceNum(), 10, 0); //if dice rolled
+        g.drawString(GameState.currentPlayer.toString() + " traded " + TradeManager.p1offer, 10, 5);
+        g.drawString(GameState.currentPlayer.toString() + " built a " , 10, 10);
+        g.drawString(GameState.currentPlayer.toString() + "bought a development card.", 10, 15);
+        g.drawString(GameState.currentPlayer.toString() + " acquired the longest army card.", 10, 20);
+        g.drawString(GameState.currentPlayer.toString() + " acquired the longest road card.", 10, 25);
+        g.drawString(GameState.currentPlayer.toString() + " rolled a seven.", 10, 30);
+        g.drawString(GameState.currentPlayer.toString() + " claimed win.", 10, 35);
     }
 }
