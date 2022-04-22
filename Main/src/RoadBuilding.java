e21c88fab99f93d79444252dc0cc643743f0b2a9
Hey-Siri <vallabhsr@gmail.com>
2022-04-22 07:31:21 -0500

    Implemented use method in RoadBuilding

--- Main/src/RoadBuilding.java
+++ Main/src/RoadBuilding.java
@@ -2,4 +2,9 @@ public class RoadBuilding extends DevelopmentCard{
     public RoadBuilding() {
         super("RoadBuilding");
     }
+    public boolean use(Edge a, Edge b) {
+        if (!Board.buildRoad(GameState.currentPlayer, a)) return false;
+        if (!Board.buildRoad(GameState.currentPlayer, b)) return false;
+        return true;
+    }
 }
