9b20efe05ddf6be4b7bb44dea552e2fff851b927
Masaab Farhan <36335899+TechnoLion-VI@users.noreply.github.com>
2022-04-28 07:20:45 -0500

    graphics in main panel sort of added

--- Main/src/MainPanel.java
+++ Main/src/MainPanel.java
@@ -1,3 +1,4 @@
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.MouseEvent;
@@ -5,17 +6,31 @@ import java.awt.event.MouseListener;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.io.PrintStream;
-import java.util.ArrayList;
+import java.util.Objects;
 
-public class MainPanel extends JPanel implements MouseListener {
+public class MainPanel extends JFrame implements MouseListener {
     private int[] xCoords;  //add values later
     private int[] yCoords;
-    private Board board;
+    //private Board;
     private JTextArea log;
+    private String playerIndStr = "PLAYER ONE";
+    private BufferedImage playerIndicator;
+    private String vicpoint1,vicpoint2,vicpoint3,vicpoint4;
+    private Font playerTitleFont;
 
-    public MainPanel() throws IOException {
-        board = new Board();
+    public MainPanel() {
+        //board = new Board();
+        try {
+            playerIndicator = ImageIO.read(Objects.requireNonNull(MainPanel.class.getResource("Images/playerIndicator.png")));
+            Font playerTitleFont = Font.createFont(0, Objects.requireNonNull(getClass().getClassLoader().getResourceAsStream("Algerian Regular.ttf"))).deriveFont(24.0F);
+            GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+            ge.registerFont(playerTitleFont);
+        } catch (Exception e) {
+            System.out.println("Exception error");
+        }
     }
+
+
 
     public void initComponents() {
         log = new JTextArea();
@@ -25,7 +40,25 @@ public class MainPanel extends JPanel implements MouseListener {
     }
 
     public void paint(Graphics g) {
-
+        g.setColor(new Color(255, 230, 153));
+        g.fillRect(0, 0, 1600, 900);
+        g.drawImage(playerIndicator, -10, -15, 385, 139, null);
+        g.setColor(Color.black);
+        g.setFont (playerTitleFont);
+        g.drawString("PLAYER ONE", 30, 75);
+        //g.setFont(myFontsmall);
+        g.drawString("PUBLIC VICTORY POINTS",5,145);
+        Graphics2D g2 = (Graphics2D) g;
+        g2.setStroke(new BasicStroke(4));
+        g2.drawLine(7,152,319,152);
+        g2.drawLine(7,152,7,255);
+        g.drawString("Player One: " + vicpoint1,11,176);
+        g.drawString("Player Two: " + vicpoint2,11,200);
+        g.drawString("Player Three: " + vicpoint3,11,224);
+        g.drawString("Player Four: " + vicpoint4,11,248);
+        g2.drawLine(15,380,525,380);
+        g2.drawLine(15,380,15,425);
+        g.drawString("PLAYED DEVELOPEMENT CARDS",15,375);
     }
 
     @Override
