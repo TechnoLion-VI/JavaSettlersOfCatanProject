b85cec6ec9a63ee9a792a9d032bdf70961fe5712
Hey-Siri <vallabhsr@gmail.com>
2022-04-28 07:20:56 -0500

    changed board to gameState in MainPanel

--- Main/src/MainPanel.java
+++ Main/src/MainPanel.java
@@ -10,11 +10,11 @@ import java.util.ArrayList;
 public class MainPanel extends JPanel implements MouseListener {
     private int[] xCoords;  //add values later
     private int[] yCoords;
-    private Board board;
+    private GameState gameState;
     private JTextArea log;
 
     public MainPanel() throws IOException {
-        board = new Board();
+        gameState = new GameState();
     }
 
     public void initComponents() {
